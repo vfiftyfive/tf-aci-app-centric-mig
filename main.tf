af1496371f6743a471b70d1e6761654fc0be03e6
vfiftyfive <vfiftyfive@gmail.com>
2020-05-15 10:27:05 +0100

    added l3out constructs

--- main.tf
+++ main.tf
@@ -18,16 +18,16 @@ resource "aci_tenant" "mig_app_centric" {
   name = var.tenant_name
 }
 
-resource "aci_vrf" "prod_vrf" {
-  tenant_dn = aci_tenant.mig_app_centric.id
-  name      = var.vrf
+data "aci_vrf" "common_vrf" {
+  tenant_dn = data.aci_tenant.common_tenant.id
+  name      = var.common_vrf
 }
 
 resource "aci_bridge_domain" "all_bds" {
   tenant_dn          = aci_tenant.mig_app_centric.id
   for_each           = var.all_bds
   name               = each.value
-  relation_fv_rs_ctx = aci_vrf.prod_vrf.name
+  relation_fv_rs_ctx = data.aci_vrf.common_vrf.name
 }
 
 resource "aci_application_epg" "all_epgs" {
@@ -38,6 +38,20 @@ resource "aci_application_epg" "all_epgs" {
   relation_fv_rs_dom_att = [data.aci_vmm_domain.vmware_vds.id]
 }
 
+data "aci_tenant" "common_tenant" {
+  name = "common"
+}
+
+data "aci_l3_outside" "l3out" {
+  tenant_dn = data.aci_tenant.common_tenant.id
+  name      = var.l3out
+}
+
+resource "aci_external_network_instance_profile" "default-ext-epg" {
+  l3_outside_dn = data.aci_l3_outside.l3out.id
+  name          = var.epg_external
+}
+
 data "aci_vmm_domain" "vmware_vds" {
   provider_profile_dn = "uni/vmmp-VMware"
   name                = var.vds_name
@@ -46,6 +60,7 @@ data "aci_vmm_domain" "vmware_vds" {
 module "web_to_order_contract" {
   source = "./modules/create_contract"
 
+  # providers = []
   tenant_id  = aci_tenant.mig_app_centric.id
   filter_map = var.web_to_order_filter_map
 }
--- variables.tf
+++ variables.tf
@@ -4,13 +4,15 @@ variable "aci_private_key" {}
 variable "aci_cert_name" {}
 variable "apic_url" {}
 variable "tenant_name" {}
-variable "vrf" {}
 variable "all_bds" {
   type = map(string)
 }
 variable "all_epgs" {
   type = map(string)
 }
+variable epg_external {}
+variable l3out {}
+variable common_vrf {}
 variable "vds_name" {}
 variable "web_to_order_filter_map" {
   type = map(object({
