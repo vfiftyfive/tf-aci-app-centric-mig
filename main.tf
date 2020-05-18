bae16b876e296d02fc7a780759d8f052240bd012
vfiftyfive <vfiftyfive@gmail.com>
2020-05-18 11:01:31 +0100

    add net_v2 module

--- main.tf
+++ main.tf
@@ -33,6 +33,22 @@ module "NET_v1_app" {
   payment_to_store_contract = var.payment_to_store_contract
 }
 
+module "NET_v2_app" {
+  source = "./modules/new_app"
+
+  tenant_name               = local.tenant_name
+  common_vrf                = local.common_vrf
+  l3out                     = local.l3out
+  app_bds                   = var.app_bds
+  app_single_bd             = local.app_single_bd
+  app_subnet                = local.app_subnet
+  app_epgs                  = var.app_epgs
+  epg_external              = var.epg_external
+  vds_name                  = var.vds_name
+  web_to_order_contract     = var.web_to_order_contract
+  order_to_payment_contract = var.order_to_payment_contract
+  payment_to_store_contract = var.payment_to_store_contract
+}
 
 
 
